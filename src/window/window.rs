bd0160b99b86a3d8c5f2784f4e29028747b29791
Willem Victor <wimpievictor@gmail.com>
2021-01-20 19:11:56 +0200

    Revert "Implement clipboard support, rework system::string API"
    This reverts commit 08f88f458772f117fe924a365a136a209e6d2153.

--- src/window/window.rs
+++ src/window/window.rs
@@ -1,5 +1,5 @@
 use crate::sf_bool_ext::SfBoolExt;
-use crate::system::{SfStrConv, Vector2i, Vector2u};
+use crate::system::{SfString, Vector2i, Vector2u};
 use crate::window::{ContextSettings, Event, Style, VideoMode};
 use csfml_system_sys::sfBool;
 use csfml_window_sys as ffi;
@@ -78,21 +78,20 @@ impl Window {
     /// * title - Title of the window
     /// * style - Window style
     /// * settings - Additional settings for the underlying OpenGL context
-    pub fn new<V: Into<VideoMode>, S: SfStrConv>(
+    pub fn new<V: Into<VideoMode>, S: Into<SfString>>(
         mode: V,
         title: S,
         style: Style,
         settings: &ContextSettings,
     ) -> Window {
+        let utf32 = title.into();
         let sf_win: *mut ffi::sfWindow = unsafe {
-            title.with_as_sfstr(|sfstr| {
-                ffi::sfWindow_createUnicode(
-                    mode.into().raw(),
-                    sfstr.as_ptr(),
-                    style.bits(),
-                    &settings.raw(),
-                )
-            })
+            ffi::sfWindow_createUnicode(
+                mode.into().raw(),
+                utf32.as_ptr(),
+                style.bits(),
+                &settings.raw(),
+            )
         };
         assert!(!sf_win.is_null(), "Failed to create Window");
         Window { window: sf_win }
@@ -207,10 +206,9 @@ impl Window {
     ///
     /// # Arguments
     /// * title - New title
-    pub fn set_title<S: SfStrConv>(&mut self, title: S) {
-        title.with_as_sfstr(|sfstr| unsafe {
-            ffi::sfWindow_setUnicodeTitle(self.window, sfstr.as_ptr())
-        })
+    pub fn set_title<S: Into<SfString>>(&mut self, title: S) {
+        let utf32 = title.into();
+        unsafe { ffi::sfWindow_setUnicodeTitle(self.window, utf32.as_ptr()) }
     }
 
     /// Show or hide a window
