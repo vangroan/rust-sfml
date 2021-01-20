7acf7c7a09bb862e0ac3d3387faec6318b8b91ee
Willem Victor <wimpievictor@gmail.com>
2021-01-20 19:11:56 +0200

    Revert "Update prerequisites section in documentation"
    This reverts commit aff23d92fc3b16819ec9e62aca43378cce649353.

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,12 @@
 //! Prerequisites
 //! =============
 //!
-//! - Rust 1.31
+//! SFML 2.4 and CSFML 2.4 must be installed on your computer. You can download them here:
 //!
-//! - SFML 2.5 and CSFML 2.5 must be installed on your computer. You can download them here:
+//! - SFML 2.4: <http://www.sfml-dev.org/download.php>
+//! - CSFML 2.4: <http://www.sfml-dev.org/download/csfml/>
 //!
-//!     - SFML 2.5: <http://www.sfml-dev.org/download.php>
-//!     - CSFML 2.5: <http://www.sfml-dev.org/download/csfml/>
-//!
-//! - Supported platforms:
-//!     - Linux
-//!     - Windows
-//!     - Mac OS X
+//! The supported platforms are Linux, Windows and OS X.
 //!
 //! # License
 //!
